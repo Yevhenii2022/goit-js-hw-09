75c094b2062a44db0a3076dced0b62040ae16919
Yevhenii2022 <3starosta3@gmail.com>
2023-03-15 19:32:39 +0200

    add task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+import { getRandomHexColor } from './helpers/random-color';
 
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+
+let intervalId = null;
+
+stopBtnEl.disabled = true;
+
+startBtnEl.addEventListener('click', handleStartBtnElClick);
+stopBtnEl.addEventListener('click', handleStopBtnElClick);
+
+function handleStartBtnElClick() {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startBtnEl.disabled = true;
+  stopBtnEl.disabled = false;
+}
+
+function handleStopBtnElClick() {
+  clearInterval(intervalId);
+  document.body.style.backgroundColor = '#ffffff';
+  startBtnEl.disabled = false;
+  stopBtnEl.disabled = true;
+}
