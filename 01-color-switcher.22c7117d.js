4bc6a79d8711b6fc451af2ed3d9340ab09bfe347
Yevhenii2022 <114651264+Yevhenii2022@users.noreply.github.com>
2023-03-15 17:33:07 +0000

    Deploying to gh-pages from @ Yevhenii2022/goit-js-hw-09@75c094b2062a44db0a3076dced0b62040ae16919 🚀

---
+++ 01-color-switcher.22c7117d.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),d=null;e.disabled=!0,t.addEventListener("click",(function(){d=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(d),document.body.style.backgroundColor="#ffffff",t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.22c7117d.js.map
