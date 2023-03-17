ef2be6fffa3aef6a1357c44901cf0b4c5552bba6
Yevhenii2022 <3starosta3@gmail.com>
2023-03-17 16:31:38 +0200

    add task-3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('submit', handleFormElSubmit);
+
+function handleFormElSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = formEl.elements;
+
+  const position = Number(amount.value);
+
+  for (let i = 0; i < position; i += 1) {
+    const delayCount = Number(delay.value) + Number(step.value) * i;
+
+    createPromise(i + 1, delayCount)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+  }
+
+  formEl.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
